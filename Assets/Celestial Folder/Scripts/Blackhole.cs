51419fdaa5be7a1c3e846976826beef56cc8bcea
SimonB <simonthedankone@gmail.com>
2021-07-27 21:31:08 +0200

    Revert "Initial commit"
    This reverts commit fbd02968b1f270424d76415fd98018f016628988.

---
+++ Assets/Celestial Folder/Scripts/Blackhole.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blackhole : Gravity
+{
+    public float checkSphereRadius = 10f;
+
+    // Update is called once per frame
+
+    override public void FixedUpdate()
+    {
+        rb.mass = mass;
+        foreach(Gravity attractor in gravityObjects){
+            if(attractor != this){
+                Attract(attractor);
+            }
+        }
+        Collider[] checkSphere = Physics.OverlapSphere(rb.position, checkSphereRadius);
+        if(checkSphere != null){
+            foreach(Collider i in checkSphere){
+                float distance = (rb.position - i.transform.position).magnitude;
+                i.transform.localScale = i.transform.localScale * distance/checkSphereRadius;
+                //IF PLANET (I) LOCALSCALE <= BLACKHOLE LOCALSCALE. PROBLEM FLOATING POINT ERROR.
+                // if(i.transform.localScale <= rb.transform.localScale){
+                //     rb.mass+=i.gameObject.GetComponent<Gravity>().mass;
+                //     Destroy(i);
+                //     //Change size of blackhole to visually grow bigger.
+                // }
+            }
+        }
+    }
+}
+
---
+++ Assets/Celestial Folder/Scripts/TrajectoryPredictor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TrajectoryPredictor : MonoBehaviour
+{
+    //MULTISCENE PHSYICS. PREDICTION HERE!!!
+}
