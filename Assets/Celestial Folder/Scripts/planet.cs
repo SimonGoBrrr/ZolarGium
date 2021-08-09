c9bb43e3f36e5bc7a7770a3d57c76a59f3264f52
SimonB <simonthedankone@gmail.com>
2021-08-09 19:23:50 +0200

    Implemented Marching Cubes
    Currently only cube formation.
    And kind of blocky, unsure if I want to have it smooth or blocky like. Time will tell. If want smooth calculate midpoint to more accurately represent  surfaceLevel.
    BUT it is implemented.
    Fantastic article joppe min!
    Also some other sources but heavily relied on this exact one.

--- Assets/Celestial Folder/Scripts/planet.cs
+++ Assets/Celestial Folder/Scripts/planet.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class planet : generation
 {
-    
+    [SerializeField]
+    private Vector3 size = new Vector3(10,10,10);
+    [SerializeField]
+    private float noiseScale = 0.1f;
+    [SerializeField]
+    private float surfaceLevel = 0.5f;
+    [SerializeField]
+    private int seed = 100;
+
+
+    void Start(){
+        var objectMesh = GetComponent<MeshFilter>();
+        var meshCollider = GetComponent<MeshCollider>();
+        Mesh marchMesh = createMesh(size, surfaceLevel, noiseScale, seed);
+        objectMesh.mesh = marchMesh;
+        meshCollider.sharedMesh = marchMesh;
+    }
 }
